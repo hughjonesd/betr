5eb57d7dda02d36cf15d6434e03566f43ec6a13e
David Hugh-Jones <davidhughjones@gmail.com>
2014-05-17 23:05:10 +0100

    new public goods game

--- inst/examples/public-goods.R
+++ inst/examples/public-goods.R
@@ -1,78 +1,63 @@
 
 library(betr)
-library(brew)
 
 # parameters
 N <- 4
-periods <- 10
+periods <- 2
 multiplier <- 1.6
 partner <- FALSE # FALSE for stranger design, TRUE for partner
-groupsize <- 4
+groupsize <- 2
+max_contrib <- 50
+timeout <- 60
 
-brewdir <- system.file("examples", package="betr")
-Nt <- N * periods
-expt <- experiment(N=N, clients_in_url=TRUE, name="public-goods", autostart=TRUE)
-with(environment(expt), {
-  mydf <- data.frame(id=rep(1:N, periods), period=rep(1:periods, each=N), 
-        group=NA_integer_, contrib=NA_integer_, timed_out=NA, earnings=NA_real_,
-        final_earnings=NA_real_)
+initialize <- function() {
+  mydf <<- experiment_data_frame(expt, group=NA, contrib=NA, 
+        timed_out=FALSE, earnings=NA, final_earnings=NA, payment_period=NA)
   for (i in 1:periods) {
-    mydf$group[mydf$period==i] <- if (i==1 || ! partner) 
-          sample(rep(1:(N/groupsize), groupsize)) else mydf$group[mydf$period==1]
+    mydf$group[mydf$period==i] <<- if (i==1 || ! partner) 
+      sample(rep(1:(N/groupsize), groupsize)) else mydf$group[mydf$period==1]
   }
-  payment_period <- NA
+}
+expt <- experiment(N=N, clients_in_url=TRUE, name="public-goods", 
+      autostart=TRUE, on_ready=initialize)
+  
+payment_period <- NA
+
+myinstructions <- text_stage(text=c(header(), "Instructions go here", footer()), 
+      wait=TRUE)
+
+myform <- form_stage(
+      form_page=c(header(), "<p>Pick a contribution:</p>",
+        "<form action='' method='POST'>", sprintf("<input type='number' 
+        name='contrib' min='0' max='%s' step='1' autocomplete='off'/>", 
+        max_contrib), "<input type='submit' value='Submit' /></form>", footer()),
+    fields=list(contrib=all_of(is_whole_number(), is_between(0, max_contrib)))
+  , data_frame="mydf")
+  
+# myform <- timed(myform, timeout, on_timeout=function(id, period) {
+#   mydf$timed_out[mydf$id==id & mydf$period==period] <<- TRUE
+# })
+  
+myprog <- program("first", function(id, period) {
+  tmp <- mydf[mydf$period==period,]
+  tmp$contrib <<- as.numeric(tmp$contrib)
+  profit <- ave(tmp$contrib, tmp$group, FUN=function(x) max_contrib - x + 
+        multiplier * mean(x))
+  profit[tmp$timed_out] <- 0
+  mydf$earnings[mydf$period==period] <<- profit
 })
-
-
-s1 <- structured_stage(
-  form = file(file.path(brewdir, "public-goods-form.brew")),
-  
-  process = function (id, period, params) {
-    me <- mydf$id==id & mydf$period==period
-    if (! "contrib" %in% names(params)) stop("Please pick a contribution")
-    contrib <- as.numeric(params$contrib)  
-    if (! contrib %in% 0:10) stop("Please pick a contribution between 0 and 10")
-    mydf$contrib[me] <<- contrib
-    mydf$timed_out[me] <<- FALSE
-  },
-  
-  timeout = 120,
-  
-  on_timeout = function(id, period) {
-    me <- mydf$id==id & mydf$period==period
-    mydf$contrib[me] <<- 0
-    mydf$timed_out[me] <<- TRUE
-  },
-  
-  wait_for = function (...) {
-    mygroup <- mydf$group[mydf$id==id & mydf$period==period]
-    all(mydf$id[mydf$group==mygroup & mydf$period==period] 
-        %in% ready)
-  },
-  
-  result = function (id, period, params) {
-    me <- mydf$id==id & mydf$period==period
-    mygroup <- mydf$group==mydf$group[me] & mydf$period==period
-    mydf$group.contrib[me] <<- sum(mydf$contrib[mygroup])
-    mydf$earnings[me] <<- 10 - mydf$contrib[me] + mydf$group.contrib[me] * 
-          multiplier / groupsize
-    if (mydf$timed_out[me]) mydf$earnings[me] <<- 0
-    capture.output(brew(file.path(brewdir, "public-goods-result.brew")))
-  }
-)
 
 sfinal <- stage(function(id, period, params) {
   if (is.na(payment_period)) {
-    payment_period <<- sample(periods, 1)
-    mydf$final_earnings[mydf$period==period] <<- mydf$earnings[
-          mdf$period==payment_period]
+    payment_period <- sample(1:periods, 1)
+    mydf$payment_period <<- payment_period
+    mydf$final_earnings <<- mydf$earnings[mydf$period==payment_period]
   }
-  final_earnings <- mydf$final_earnings[mydf$id==id & mydf$period==period]
-  capture.output(brew(file.path(brewdir, "public-goods-final.brew")))
+  #capture.output(brew(file.path(brewdir, "public-goods-final.brew")))
+  c(header(), sprintf("<p>You earned $%2f</p>", mydf$final_earnings), footer())
 })
 
-add_stage(expt, s1, times=periods)
+add_stage(expt, myinstructions)
+add_stage(expt, period(), myform, checkpoint("all"), myprog, times=periods)
 add_stage(expt, sfinal)
-
-cat("Call ready(expt) to begin waiting for clients.", file=stderr())
   
