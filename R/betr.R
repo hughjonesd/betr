c1c2e77ed8f5ce7672c5e5c0211a344c7a622f2c
David Hugh-Jones <davidhughjones@gmail.com>
2014-11-18 12:13:10 +0000

    add elapsed time to info

--- R/betr.R
+++ R/betr.R
@@ -248,10 +248,11 @@ Experiment <- setRefClass("Experiment",
     get_url = function() server$get_url(),
         
     info = function(subj=TRUE, map=TRUE) {
-      cat(sprintf("%s\tStatus: %s\tClients: %d/%0.0f\tPeriods: %d\tStages: %d\n", 
+      cat(sprintf(
+            "%s\tStatus: %s\tElapsed time:%0.1f\nClients: %d/%0.0f\tPeriods: %d\tStages: %d\n", 
             ifelse(status=="Stopped", paste("Name:", name), paste("Session:",
-            session_name)), status, nrow(subjects), N, nperiods(), 
-            length(stages)))
+            session_name)), status, elapsed_time(), nrow(subjects), N, 
+            nperiods(), length(stages)))
       if (status != "Stopped") server$info()
       if (subj && nrow(subjects) > 0) {
         cat("Subjects:\n")
